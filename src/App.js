5dc84f5ec39355eabb4e35c3fbb604dfe1689f95
Danielbzg <daniel.bozagarcia@gmail.com>
2022-03-28 13:35:27 +0200

    NavBar añadido

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Serendipia</h1>
         <button>Boton</button>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+        <nav className="NavBar">
+            <button>Inicio</button>
+            <button>Quiénes somos</button>
+            <button>Productos</button>
+            <button>Contacto</button>
+        </nav>
+    )
+}
+
+export default NavBar
