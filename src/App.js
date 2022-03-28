4cb1baa53ea60e03ed7ba53834cb690b114457c6
Danielbzg <daniel.bozagarcia@gmail.com>
2022-03-28 02:12:21 +0200

    Clase de instalacion de entorno

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Serendipia</h1>
+        <button>Boton</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
