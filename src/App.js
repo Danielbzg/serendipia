eb45b4e2859f4eefa2b998866ede593c11e131d1
Danielbzg <daniel.bozagarcia@gmail.com>
2022-04-05 19:47:42 +0200

    Desafío del contador de productos en stock corregido

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
 
 const App = () => {
   
